9e0fcb3c8efc625397e380e3b9c1f70fa436ef39
solarconstant <ashishshubh001@gmail.com>
2020-09-02 11:01:48 +0530

    added start menu and changed apple

--- Intro.py
+++ Intro.py
@@ -16,9 +16,13 @@ display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('Slither')
+pygame.display.set_caption('Snake')
+
+icon = pygame.image.load('samosa.png')
+pygame.display.set_icon(icon)
 
 img = pygame.image.load('head.png')
+samosa = pygame.image.load('samosa.png')
 clock = pygame.time.Clock()
 block_size = 30
 FPS = 20
@@ -27,6 +31,43 @@ direction = 'right'
 smallfont = pygame.font.SysFont('helvetica', 25)
 medfont = pygame.font.SysFont('helvetica', 50)
 largefont = pygame.font.SysFont('helvetica', 80)
+
+def game_intro():
+	intro = True
+	while intro:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+				elif event.key == pygame.K_c:
+					intro = False
+
+		gameDisplay.fill(color)
+		message_to_screen('Welcome to snake',
+							(130, 180, 90),
+							-100,
+							size = 'large')
+		message_to_screen('Eat the red apples.',
+							(0, 0, 0),
+							-30)
+		message_to_screen('The more apples you eat, the longer you get.',
+							(0, 0, 0),
+							10)
+		message_to_screen('Don\'t run into yourself or the border.',
+							(0, 0, 0),
+							40)
+		message_to_screen('Press C to play or Q to quit.',
+							(240, 20, 0),
+							90,
+							size = 'medium')
+		pygame.display.update()
+		clock.tick(30)
 
 def snake(block_size, snake_list):
 	if direction == 'right':
@@ -40,7 +81,7 @@ def snake(block_size, snake_list):
 
 	gameDisplay.blit(head, (snake_list[-1][0], snake_list[-1][1]))
 
-	for XnY in snake_list[:-1]:
+	for XnY in snake_list[:-3]:
 		pygame.draw.rect(gameDisplay, (114, 194, 33), [XnY[0], XnY[1], block_size, block_size])
 
 def text_objects(text, color, size):
@@ -115,8 +156,8 @@ def gameloop():
 		lead_x += lead_x_change
 		lead_y += lead_y_change
 		gameDisplay.fill(color)
-		pygame.draw.rect(gameDisplay, (0, 255, 0), [position_apple_X, position_apple_Y, block_size, block_size])
-
+		#pygame.draw.rect(gameDisplay, (0, 255, 0), [position_apple_X, position_apple_Y, block_size, block_size])
+		gameDisplay.blit(samosa, (position_apple_X, position_apple_Y))
 		snake_head = []
 		snake_head.append(lead_x)
 		snake_head.append(lead_y)
@@ -139,12 +180,13 @@ def gameloop():
 
 		if lead_x >= position_apple_X and lead_x <= position_apple_X + block_size or lead_x + block_size >= position_apple_X and lead_x + block_size <= position_apple_X + block_size:
 			if lead_y >= position_apple_Y and lead_y <= position_apple_Y + block_size or lead_y + block_size >= position_apple_Y and lead_y + block_size <= position_apple_Y + block_size:
-				snake_length += 2
+				snake_length += 3
 				position_apple_X = round(random.randrange(0, display_width - block_size))
 				position_apple_Y = round(random.randrange(0, display_height - block_size))
 		clock.tick(FPS)
 	pygame.quit()
 	quit()
 
+game_intro()
 gameloop()
 	
